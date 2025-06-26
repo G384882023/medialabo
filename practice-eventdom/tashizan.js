4a9caef15c08b932c1b02ccef2a5936de32422bd
G384882023 <238488@st.takushoku-u.ac.jp>
2025-06-26 18:18:15 +0900

    変更

--- practice-eventdom/tashizan.js
+++ practice-eventdom/tashizan.js
@@ -1,8 +1,8 @@
 function greeting3(){
     let i = document.querySelector('input[name="left"]');
-    let leftbox = parseInt(i.value);
+    let leftbox = Number(i.value);
     let j = document.querySelector('input[name="right"]');
-    let rightbox = parseInt(j.value);
+    let rightbox = Number(j.value);
         let sum = leftbox + rightbox;
         let p = document.querySelector('#answer');
         p.textContent = sum;
