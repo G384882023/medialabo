460c8c6df81965ccbb2064b5b60f7193a3a4f156
G384882023 <238488@st.takushoku-u.ac.jp>
2025-07-03 15:06:52 +0900

    変更

--- practice-dom2/dom2-intro.js
+++ practice-dom2/dom2-intro.js
@@ -1,29 +1,29 @@
-    b = document.querySelector('button#henkou');
-    b.addEventListener('click', changeDom);
-
 changeDom(){
-    let l = document.createElement('li'); //ヨットを追加する
-    l.textContent = 'ヨット';
+    //ヨットを追加する
+    let l = document.createElement('li');
     let u = document.querySelector('ul#kazoeuta');
     u.insertAdjacentElement('beforeend', l);
+    l.textContent = 'ヨット';
 
-
+    //ブルームーンの画像を貼る
     let i = document.querySelector('img#bluemoon'); // 要素の検索
     i.setAttribute('src', 'bluemoon.jpg'); // 属性の設定
 
-
+    //拓大リンクの追加
     let a = document.createElement('a'); // 新規要素 a を作成
     a.textContent = '拓殖大学HP'; // 要素 a のテキスト設定
     a.setAttribute('href', 'https://www.takushoku-u.ac.jp'); // 属性 href の設定
     let p = document.querySelector('p#takudai');
     onabort.insertAdjacentElement('afterend', a); // p の直後に a を追加
 
-
+    //「餅」を削除
     l = document.querySelector('li#mochi'); // 削除したい要素を検索
     l.remove();                             // 要素を削除
-    u = document.querySelector('ul#kassen'); // ul 要素を検索
+    //「ul"kassen」を削除                           
+    u = document.querySelector('ul#kassen');
     u,remove();
-
+    
+    //光の三原色追加
     u = document.createElement('ul'); //新規要素の作成
     p = document.querySelector('p#primary');
     p.insertAdjacentElement('afterend', u);
@@ -41,3 +41,5 @@ changeDom(){
     l.textContent = '青';
 
 }
+b = document.querySelector('button#henkou');
+b.addEventListener('click', changeDom);
